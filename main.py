85684d23d8be72c12d8ccd3babb2f9e43a35799b
He11evenB <he11evenb@gmail.com>
2024-10-20 16:23:52 -0700

    added new function character count

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ def main():
     book_path = "books/frankenstein.txt"
     print(get_book_text(book_path))
     print(get_word_count(book_path))
-
+    print(get_char_count(book_path))
 
 def get_book_text(text):
     with open(text) as book:
         return book.read()
 
 def get_word_count(text):
-    with open(text) as book:
-        return len(book.read().split())
+    return len(get_book_text(text).split())
+
+def get_char_count(text):
+    char_count = {chr(i): 0 for i in range(97, 123)}
+    char_case = get_book_text(text).lower()
+    for char in char_case:
+        if char in char_count:
+            char_count[char] += 1
+    return char_count
 
 main()
