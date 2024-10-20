ff7a976a7e75789aa79d9306c2e7872c6e61f771
He11evenB <he11evenb@gmail.com>
2024-10-20 16:47:31 -0700

    added a report function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     print(get_book_text(book_path))
     print(get_word_count(book_path))
     print(get_char_count(book_path))
+    print(create_report(book_path))
 
 def get_book_text(text):
     with open(text) as book:
@@ -18,5 +19,20 @@ def get_char_count(text):
         if char in char_count:
             char_count[char] += 1
     return char_count
+
+def create_report(text):
+    wc = get_word_count(text)
+    cc = get_char_count(text)
+
+    print(f"--- Begin report of {text} ---")
+    print(f"{wc} words found in the document")
+    print()
+    
+    sorted_char_count = sorted(cc.items(), key=lambda item: item[1], reverse=True)
+
+    for char, count in sorted_char_count:
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End report ---")
 
 main()
